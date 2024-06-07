e14e589768e04e1cdfa2aac5ddb60cef92bab5d9
yangbo568969859 <568969859@qq.com>
2024-06-07 17:40:06 +0800

    feat: 问题排查

--- src/utils/content.dynamic.js
+++ src/utils/content.dynamic.js
@@ -54,6 +54,8 @@ const getContentRouter = async () => {
   const _getMarkdownFile = async (pathname = '') => {
     const normalizedPathname = normalize(pathname).replace('.', '');
     console.log('process.cwd()', process.cwd(), join(process.cwd(), 'public/content', `AI/base.md`));
+    const data = await readFile(join(process.cwd(), 'public/content', `AI/base.md`))
+    console.log('文件内容:', data);
     console.log('特殊文件', existsSync(join(process.cwd(), 'public/content', `AI/base.md`)))
     console.log('normalizedPathname', normalizedPathname);
     console.log('_getMarkdownFile pathnameToFilename.has(normalizedPathname)', pathnameToFilename.has(normalizedPathname))
