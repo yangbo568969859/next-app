997076bc078ae44d5e28c54358443d1a197f2a03
yangbo568969859 <568969859@qq.com>
2024-06-13 14:21:43 +0800

    feat: 时间格式化

--- src/components/withSiteContentInfo.tsx
+++ src/components/withSiteContentInfo.tsx
@@ -1,14 +1,27 @@
 import { FC } from 'react';
 import { Icons } from '@/src/components/UIBase/Icons'
 
+function getDate(dateString: string) {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+
+  return `${year}-${month}-${day}`;
+}
+
 const WithSiteContentInfo: FC<any> = (props: any) => {
   const { meta, readingTime } = props;
+  let pageShowDate = '';
+  if (meta && meta.date) {
+    pageShowDate = getDate(meta.date);
+  }
   return (
     <div className='flex text-xs mb-4'>
       {
         meta && meta.date ? <div className='mr-3 flex items-center'>
           <Icons.calendar size={14} className='mr-1' />
-          {meta.date.toString()}
+          {pageShowDate}
         </div> : null
       }
       {
