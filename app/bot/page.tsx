761b87c13a245ccd2c6c36207c57735831366da3
yangbo568969859 <568969859@qq.com>
2025-04-10 23:11:25 +0800

    fix: 问题修复

--- app/bot/page.tsx
+++ app/bot/page.tsx
@@ -1,21 +1,22 @@
 'use client'
 import React, { useEffect } from 'react';
 import WithSiteHeader from '@/src/components/withSiteHeader';
-import { update } from '@/src/thirdPlugin/script';
+// import { update } from '@/src/thirdPlugin/script';
+import LayoutFluid from '@/src/components/layoutFluid';
 
 const BotPage = () => {
 
   useEffect(() => {
-    setTimeout(() => {
-      console.log(update, 'update');
-    }, 1000);
+    // setTimeout(() => {
+    //   console.log(update, 'update');
+    // }, 1000);
   }, [])
   return (
     <div className="w-full relative h-[calc(100dvh-4rem-1px)]">
       <WithSiteHeader></WithSiteHeader>
       <div className='absolute inset-0 overflow-hidden motion-reduce:invisible motion-safe:visible'>
         <div className='fixed top-0 left-0 z-5 pointer-events-none'>
-          <canvas id='fluid' className='w-screen h-screen'></canvas>
+          <LayoutFluid />
         </div>
       </div>
       <div className='relative z-10 w-full h-full flex items-center justify-center'>
